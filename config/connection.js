6fbbcb8f97fb3bab0f3217ce5ad0c63219cae9b5
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-12-04 18:54:15 -0800

    added CLEARDB .env

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(
   "burgers_db",
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_USER || CLEARDB_USER,
+  process.env.DB_PASS || CLEARDB_PASS,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || process.env.CLEARDB_HOST,
     port: 3306,
     dialect: "mysql",
     pool: {
