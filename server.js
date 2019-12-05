1ea3ba279aef1385adcbd38418732afdebf4a14d
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-12-05 15:50:43 -0800

    retest

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-console.log(process.env.JAWSDB_DATABASE,
-  process.env.JAWSDB_USER,
-  process.env.JAWSDB_PASS,);
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
