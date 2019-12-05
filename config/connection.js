83e48a0ba37f68d2be20fbb787800d1771c9206a
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-12-04 19:44:48 -0800

    changed PORT test

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const PORT = process.env.PORT || 3306;
 require("dotenv").config();
 
 const sequelize = new Sequelize(
@@ -7,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    port: 3306,
+    port: PORT,
     dialect: "mysql",
     pool: {
       max: 5,
